216d4e93cde807659d53d7fc08ce98929627313b
Jaavan Greene <jaavan.greene@flex.com>
2020-06-17 12:44:29 -0400

    fixing comment blocks

--- markdownconverter/src/services/OutputConverter.js
+++ markdownconverter/src/services/OutputConverter.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 export class OutputConverter {
-    // Appending markup to the DOM in this manner is generally frowned upon but for the purpose of this example project I'll do it. *
+    /* 
+        Appending markup to the DOM in this manner is generally frowned upon 
+        but for the purpose of this example project I'll do it.
+    */
     static ConvertToHtml(input) {
         let converted = this.convertMarkdown(input);
         return <div dangerouslySetInnerHTML={this.createMarkup(converted)} />;
@@ -31,21 +34,29 @@ export class OutputConverter {
         and turns all of the text of that line into a paragraph tag
     */
     static convertParagraphs(input) {
-        //replaces any lines that don't start with a header tag with a paragraph
-        //it is possible that lines start with a link and only contain said link. This will be cleaned later
+        /* 
+            replaces any lines that don't start with a header tag with a paragraph
+            it is possible that lines start with a link and only contain said link. 
+            This will be cleaned later
+        */
         let regPara = new RegExp(`(^(?!#{1,6}\\s{1})).+`, 'gm')
         input = input.replace(regPara, `<p>$&</p>`);
         return input;
     }
 
     /*
-        Header conversion checks that you have anywhere  from 1 to 6 #'s with at least one space followed by the header text
+        Header conversion checks that you have anywhere  from 1 to 6 #'s with at least one space 
+        followed by the header text
     */
     static convertHeaders(input) {
         for (var i = 6; i >= 1; i--) {
-            // selects characters matching '#n {anything but a line return} where n is the number of #'s and the corresponding header level.'
+            /* 
+                selects characters matching '#n {anything but a line return} where n is the number of #'s and the corresponding header level.'
+            */
             let regSelect = new RegExp(`^#{${i}}\\s{1}(.+)`, 'gm')
-            // replaces the entire match with just the sectioned off portion being our inner value
+            /* 
+                replaces the entire match with just the sectioned off portion being our inner value
+            */
             input = input.replace(regSelect, `<h${i}>$1</h${i}>`);
         }
         return input;
